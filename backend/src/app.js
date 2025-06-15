a2e9672f97c7a67ff8ef3b799a4d9d6c9f3b6e81
Sanket Gaikwad <sanketlike63@gmail.com>
2025-06-15 18:03:05 +0530

    fix api

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,8 +21,5 @@ app.use('/api/job-applications', require('./routes/jobApplication.routes'));
 app.use('/api/campus-jobs', require('./routes/campushJob.routes'));
 app.use('/api/job-scraped', require('./routes/jobScraped.routes'));
 app.use('/api/campus-job-applications', require('./routes/campusJobApplication.routes'));
-app.get('/', (req, res) => {
-  res.send('Job Portal Backend Running');
-});
 
 module.exports = app;
